b1927d9bc0e85d8172ed6097d684c1a9ed9341db
Cheuk Yin Ng <chucksys88@gmail.com>
2018-06-20 14:57:49 -0700

    Refactors everything into a folder

--- handler.js
+++ handler.js
@@ -5,133 +5,133 @@ $("#btn_off").on("click",function(){
 
 
 $("#albert").on("click",function(){
-	var sound = new Howl({src: ['albert.mp3']}).play();
+	var sound = new Howl({src: ['sounds/albert.mp3']}).play();
 });
 
 $("#blup").on("click",function(){
-	var sound = new Howl({src: ['blup.mp3']}).play();
+	var sound = new Howl({src: ['sounds/blup.mp3']}).play();
 });
 
 $("#fed").on("click",function(){
-	var sound = new Howl({src: ['feed.mp3']}).play();
+	var sound = new Howl({src: ['sounds/feed.mp3']}).play();
 });
 
 $("#hungry").on("click",function(){
-	var sound = new Howl({src: ['hungry.mp3']}).play();
+	var sound = new Howl({src: ['sounds/hungry.mp3']}).play();
 });
 
 $("#jaime").on("click",function(){
-	var sound = new Howl({src: ['jeimi.mp3']}).play();
+	var sound = new Howl({src: ['sounds/jeimi.mp3']}).play();
 });
 
 $("#nani").on("click",function(){
-	var sound = new Howl({src: ['nani.mp3']}).play();
+	var sound = new Howl({src: ['sounds/nani.mp3']}).play();
 });
 
 $("#okay").on("click",function(){
-	var sound = new Howl({src: ['okay.mp3']}).play();
+	var sound = new Howl({src: ['sounds/okay.mp3']}).play();
 });
 
 $("#omg").on("click",function(){
-	var sound = new Howl({src: ['omg.mp3']}).play();
+	var sound = new Howl({src: ['sounds/omg.mp3']}).play();
 });
 
 $("#tired").on("click",function(){
-	var sound = new Howl({src: ['tired.mp3']}).play();
+	var sound = new Howl({src: ['sounds/tired.mp3']}).play();
 });
 
 $("#scream").on("click",function(){
-	var sound = new Howl({src: ['scream.mp3']}).play();
+	var sound = new Howl({src: ['sounds/scream.mp3']}).play();
 });
 
 $("#spin").on("click",function(){
-	var sound = new Howl({src: ['spin.mp3']}).play();
+	var sound = new Howl({src: ['sounds/spin.mp3']}).play();
 });
 
 $("#honestly").on("click",function(){
-	var sound = new Howl({src: ['honestly.mp3']}).play();
+	var sound = new Howl({src: ['sounds/honestly.mp3']}).play();
 });
 
 $("#berry").on("click",function(){
-	var sound = new Howl({src: ['berry.mp3']}).play();
+	var sound = new Howl({src: ['sounds/berry.mp3']}).play();
 });
 
 $("#pic").on("click",function(){
-	var sound = new Howl({src: ['pic.mp3']}).play();
+	var sound = new Howl({src: ['sounds/pic.mp3']}).play();
 });
 
 $("#ah").on("click",function(){
-	var sound = new Howl({src: ['ah.mp3']}).play();
+	var sound = new Howl({src: ['sounds/ah.mp3']}).play();
 });
 
 $("#evilha").on("click",function(){
-	var sound = new Howl({src: ['evilha.mp3']}).play();
+	var sound = new Howl({src: ['sounds/evilha.mp3']}).play();
 });
 
 $("#yikes2").on("click",function(){
-	var sound = new Howl({src: ['yikes2.mp3']}).play();
+	var sound = new Howl({src: ['sounds/yikes2.mp3']}).play();
 });
 
 $("#cream").on("click",function(){
-	var sound = new Howl({src: ['cream.mp3']}).play();
+	var sound = new Howl({src: ['sounds/cream.mp3']}).play();
 });
 
 $("#Pichu").on("click",function(){
-	var sound = new Howl({src: ['pichuu.mp3']}).play();
+	var sound = new Howl({src: ['sounds/pichuu.mp3']}).play();
 });
 
 $("#munimuni").on("click",function(){
-	var sound = new Howl({src: ['munimuni.mp3']}).play();
+	var sound = new Howl({src: ['sounds/munimuni.mp3']}).play();
 });
 
 $("#meanit").on("click",function(){
-	var sound = new Howl({src: ['meanit.mp3']}).play();
+	var sound = new Howl({src: ['sounds/meanit.mp3']}).play();
 });
 
 $("#hug").on("click",function(){
-	var sound = new Howl({src: ['hug.mp3']}).play();
+	var sound = new Howl({src: ['sounds/hug.mp3']}).play();
 });
 
 $("#Pikachu").on("click",function(){
-	var sound = new Howl({src: ['pikachu.mp3']}).play();
+	var sound = new Howl({src: ['sounds/pikachu.mp3']}).play();
 });
 
 $("#listen").on("click",function(){
-	var sound = new Howl({src: ['listen.mp3']}).play();
+	var sound = new Howl({src: ['sounds/listen.mp3']}).play();
 });
 
 $("#yikes1").on("click",function(){
-	var sound = new Howl({src: ['yikes1.mp3']}).play();
+	var sound = new Howl({src: ['sounds/yikes1.mp3']}).play();
 });
 
 $("#norha").on("click",function(){
-	var sound = new Howl({src: ['norha.mp3']}).play();
+	var sound = new Howl({src: ['sounds/norha.mp3']}).play();
 });
 
 $("#kirby").on("click",function(){
-	var sound = new Howl({src: ['kirby.mp3']}).play();
+	var sound = new Howl({src: ['sounds/kirby.mp3']}).play();
 });
 
 $("#heh").on("click",function(){
-	var sound = new Howl({src: ['heh.mp3']}).play();
+	var sound = new Howl({src: ['sounds/heh.mp3']}).play();
 });
 
 $("#punch").on("click",function(){
-	var sound = new Howl({src: ['onepunch.mp3']}).play();
+	var sound = new Howl({src: ['sounds/onepunch.mp3']}).play();
 });
 
 $("#humps").on("click",function(){
-	var sound = new Howl({src: ['humps.mp3']}).play();
+	var sound = new Howl({src: ['sounds/humps.mp3']}).play();
 });
 
 $("#notice").on("click",function(){
-	var sound = new Howl({src: ['notice.mp3']}).play();
+	var sound = new Howl({src: ['sounds/notice.mp3']}).play();
 });
 
 $("#sacrifice").on("click",function(){
-	var sound = new Howl({src: ['sacrifice.mp3']}).play();
+	var sound = new Howl({src: ['sounds/sacrifice.mp3']}).play();
 });
 
 $("#smooth").on("click",function(){
-	var sound = new Howl({src: ['smooth.mp3']}).play();
+	var sound = new Howl({src: ['sounds/smooth.mp3']}).play();
 });
